da7edc2f20ad0adaae144156a3a4637c39ff4a69
JadenLews <jlewis@clarku.edu>
2024-10-19 10:10:18 -0400

    Up to date with models

--- hackathon2024/main/views.py
+++ hackathon2024/main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import ProjectPost
 # Create your views here.
 def portfolio_page(request):
     return render(request, "main/portfolio.html")
 
 def home(request):
-    return render(request, "main/home.html")
+    # Fetch all posts from the database
+    posts = ProjectPost.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, "main/home.html", context)
