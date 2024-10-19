f049b680105b92aaefa56c7b20c76481dc454982
kaipborwell <132088432+kaipborwell@users.noreply.github.com>
2024-10-19 17:14:35 -0400

    changes to docker-compose.yml and urls

--- hackathon2024/hackathon2024/urls.py
+++ hackathon2024/hackathon2024/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main import views as main_views
+from django.shortcuts import redirect
 
 from django.conf import settings
 from django.conf.urls.static import static
