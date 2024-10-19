9a5be7d744b8a69f6b95ce59f495c61c71949fdd
JadenLews <jlewis@clarku.edu>
2024-10-19 09:32:09 -0400

    Home Page and project model

--- hackathon2024/main/views.py
+++ hackathon2024/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def portfolio_page(request):
     return render(request, "main/portfolio.html")
+
+def home(request):
+    return render(request, "main/home.html")
