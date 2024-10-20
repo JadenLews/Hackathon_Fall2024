7d6acca142c66b926fd60f86ab1bac4d0534496a
JadenLews <jlewis@clarku.edu>
2024-10-20 00:19:48 -0400

    push

--- hackathon2024/main/models.py
+++ hackathon2024/main/models.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import User
 
 class ProjectPost(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
+    description_long = models.TextField(null=True, blank=True)
+    description = models.JSONField(default=list, blank=True) 
+    skills = models.JSONField(default=list, blank=True) 
     likes = models.IntegerField(default=0)
     workers = models.IntegerField(default=1)
-    workersneeded = models.IntegerField(default=0)  # This is the new field
+    workersneeded = models.IntegerField(default=2)  # This is the new field
     date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    skills = models.JSONField()
+    skills = models.JSONField(default=list, blank=True)
+    categories = models.JSONField(default=list, blank=True)  
 
     def __str__(self):
         return self.title
