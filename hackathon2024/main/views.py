6b758c8453525658f98fcb4347b725dbf5dfd5e1
JadenLews <jlewis@clarku.edu>
2024-10-20 01:29:35 -0400

    working user edit

--- hackathon2024/main/views.py
+++ hackathon2024/main/views.py
@@ -127,13 +127,14 @@ def profile(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            return redirect('profile')
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('/portfolio')  # Redirect to portfolio page after saving
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
-    
+
     context = {
         'user_form': user_form,
-        'profile_form': profile_form,
+        'profile_form': profile_form
     }
     return render(request, 'profile.html', context)
